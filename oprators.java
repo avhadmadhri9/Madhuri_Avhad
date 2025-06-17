a69d5f9b9734688cac3ae1736bd4adcdd52e015f
madhuriavhad <avhadmadhuri9@gmail.com>
2025-06-17 13:57:21 +0530

    Add files via upload

---
+++ oprators.java
@@ -0,0 +1,25 @@
+public class oprators {
+    public static void main(String[] args) {
+        
+    
+    int a=10;
+    int b=20;
+    System.out.println("a + b = " + (a + b)); 
+System.out.println("a - b="+(a-b));
+System.out.println("a*b=" +(a*b));
+System.out.println("a / b = " + (a / b)); 
+System.out.println("a % b = " + (a % b)); 
+
+        // boolean x = true;
+        // boolean y = false;
+
+        // System.out.println("x && y: " + (x && y));  
+        // System.out.println("x || y: " + (x || y));  // true
+        // System.out.println("!x: " + (!x));          // false
+    }}
+
+
+
+
+    
+
